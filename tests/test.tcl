c6ba501fcf715765a11105a73edf390846a10dd7
ify <ify.oraelosi@moonfare.com>
2021-08-24 19:31:01 +0200

    Write test spec for filling the table

--- tests/test.tcl
+++ tests/test.tcl
@@ -26,3 +26,30 @@ if {[string compare $result $expected] != 0} {
 } else {
   puts "Test passed"
 }
+
+set baseCommand ""
+set insertCommand "insert 1 foo a@b.c\n"
+
+# Bulk insert test
+set description2 "prints error message when table is full"
+set expected2 "db > Error: Table full."
+
+for { set a 0} {$a < 1401} {incr a} {
+  append baseCommand $insertCommand
+}
+
+append baseCommand ".exit\n"
+
+set result2 [exec $dbliteFileName << $baseCommand]
+set result2_list [split $result2 "\n"]
+
+set lastOutput [lindex $result2_list 1400]
+
+if {[string compare $lastOutput $expected2] != 0} {
+  puts "Test failure"
+  puts "Description:\n  $description2"
+  puts "Expected result:\n  $expected2"
+  puts "Received result:\n  $lastOutput"
+} else {
+  puts "Test passed"
+}
