b01fdb05e720cecc4c80448fc87530307e5028b6
ify <ify.oraelosi@moonfare.com>
2021-09-17 19:47:08 +0200

    Add command to print the tree (list of cells in the single node (page))

--- db.c
+++ db.c
@@ -269,6 +269,17 @@ void initialize_leaf_node(void *node)
 {
   *leaf_node_num_cells(node) = 0;
 }
+
+void print_leaf_node(void *node)
+{
+  uint32_t num_cells = *leaf_node_num_cells(node);
+  printf("leaf (size %d)\n", num_cells);
+  for (uint32_t i = 0; i < num_cells; i++)
+  {
+    uint32_t key = *leaf_node_key(node, i);
+    printf("  - %d : %d\n", i, key);
+  }
+}
 // </TREE DEFINITIONS>
 
 // display a prompt requesting input
@@ -392,31 +403,6 @@ void print_constants()
   printf("LEAF_NODE_CELL_SIZE: %d\n", LEAF_NODE_CELL_SIZE);
   printf("LEAF_NODE_SPACE_FOR_CELLS: %d\n", LEAF_NODE_SPACE_FOR_CELLS);
   printf("LEAF_NODE_MAX_CELLS: %d\n", LEAF_NODE_MAX_CELLS);
-}
-
-// Check if the input buffer holds a meta command
-MetaCommandResult do_meta_command(InputBuffer *input_buffer, Table *table)
-{
-  if (strcmp(input_buffer->buffer, ".exit") == 0)
-  {
-    db_close(table);
-    exit(EXIT_SUCCESS);
-  }
-  else if (strcmp(input_buffer->buffer, ".help") == 0)
-  {
-    printf(".exit: Exits the REPL\n");
-    return META_COMMAND_SUCCESS;
-  }
-  else if (strcmp(input_buffer->buffer, ".constants") == 0)
-  {
-    printf("Constants:\n");
-    print_constants();
-    return META_COMMAND_SUCCESS;
-  }
-  else
-  {
-    return META_COMMAND_UNRECOGNIZED_COMMAND;
-  }
 }
 
 // tokenize the insert statement using strtok
@@ -753,6 +739,37 @@ Table *db_open(const char *filename)
   }
 
   return table;
+}
+
+// Check if the input buffer holds a meta command
+MetaCommandResult do_meta_command(InputBuffer *input_buffer, Table *table)
+{
+  if (strcmp(input_buffer->buffer, ".exit") == 0)
+  {
+    db_close(table);
+    exit(EXIT_SUCCESS);
+  }
+  else if (strcmp(input_buffer->buffer, ".btree") == 0)
+  {
+    printf("Tree:\n");
+    print_leaf_node(get_page(table->pager, 0));
+    return META_COMMAND_SUCCESS;
+  }
+  else if (strcmp(input_buffer->buffer, ".help") == 0)
+  {
+    printf(".exit: Exits the REPL\n");
+    return META_COMMAND_SUCCESS;
+  }
+  else if (strcmp(input_buffer->buffer, ".constants") == 0)
+  {
+    printf("Constants:\n");
+    print_constants();
+    return META_COMMAND_SUCCESS;
+  }
+  else
+  {
+    return META_COMMAND_UNRECOGNIZED_COMMAND;
+  }
 }
 
 // main function will have an infinite loop that prints the prompt,
